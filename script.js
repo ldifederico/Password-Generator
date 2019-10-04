e4e56d024eb6832179029ee37d54d10302df0b7d
ldifederico <54997841+ldifederico@users.noreply.github.com>
2019-10-04 16:36:08 -0400

    Add files via upload
    checkboxes added

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
 //enable checkboxes
-/* var lowercaseChars = " ";
+var lowercaseChars = " ";
 var uppercaseChars = " ";
 var numericChars = " ";
 var specialChars = " ";
 
+var checkbox1 = document.getElementById("defaultCheck1");
+checkbox1.addEventListener("click", checkCheckbox1);
+
+var checkbox2 = document.getElementById("defaultCheck2");
+checkbox2.addEventListener("click", checkCheckbox2);
+
+var checkbox3 = document.getElementById("defaultCheck3");
+checkbox3.addEventListener("click", checkCheckbox3);
+
+var checkbox4 = document.getElementById("defaultCheck4");
+checkbox4.addEventListener("click", checkCheckbox4);
+
 function checkCheckbox1(){
-    var checkbox = document.getElementById(defaultCheck1);
-    if (checkbox.checked == true){
-        lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
-    } else {
-        lowercaseChars = "0";
+    console.log('we are here');
+    if (checkbox1.checked){
+        lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        console.log('we added the characters and the are ' + lowercaseChars);
     }
+    else {
+        lowercaseChars = ""
+        console.log('empty string');
+    }
+        
 }
 
 function checkCheckbox2(){
-    var checkbox = document.getElementById(defaultCheck2);
-    if (checkbox.checked == true){
+    console.log('we are here');
+    if (checkbox2.checked){
         uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    } else {
-        uppercaseChars = "0";
+        console.log('we added the characters and the are ' + uppercaseChars);
+    }
+    else {
+        uppercaseChars = ""
+        console.log('empty string');
     }
 }
 
 function checkCheckbox3(){
-    var checkbox = document.getElementById(defaultCheck3);
-    if (checkbox.checked == true){
+    console.log('we are here');
+    if (checkbox3.checked){
         numericChars = "0123456789"
-    } else {
-        numericChars = "0";
+        console.log('we added the characters and the are ' + numericChars);
+    }
+    else {
+        numericChars = ""
+        console.log('empty string');
     }
 }
 
 function checkCheckbox4(){
-    var checkbox = document.getElementById(defaultCheck4);
-    if (checkbox.checked == true){
+    console.log('we are here');
+    if (checkbox4.checked){
         specialChars = "!#$%&'()*+,-./:;=>?@[^_`{|}~"
-    } else {
-        specialChars = "0";
+        console.log('we added the characters and the are ' + specialChars);
     }
-}  */
+    else {
+        specialChars = ""
+        console.log('empty string');
+    }
+}  
 
 alert("Enter desired password criteria. If no criteria are selected, all will be included.")
 //generate random password
@@ -47,8 +72,7 @@ function generatePassword(){
     //select password length
     var length = document.getElementById("slider").value;
     
-    // var values = lowercaseChars + uppercaseChars + numericChars + specialChars;
-    var values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+    var values = lowercaseChars + uppercaseChars + numericChars + specialChars;
     var password = "";
 
     //loop
@@ -83,4 +107,3 @@ function copyPassword(){
     document.execCommand("Copy");
     alert("Password copied to clipboard!");
 }
-
